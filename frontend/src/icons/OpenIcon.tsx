4ce114ce460ff85869306ef6b9a293f4aa6da2c6
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-05-15 16:16:47 +0530

    fix: update SVG icon attributes to use camelCase; ensure consistency across icon components

--- frontend/src/icons/OpenIcon.tsx
+++ frontend/src/icons/OpenIcon.tsx
@@ -6,13 +6,13 @@ export default function OpenIcon(props: IconProps) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="2"
       stroke="currentColor"
       className={IconSizeVariants[props.size]}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
       />
     </svg>

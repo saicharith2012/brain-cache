8b862f8705f5218c7d58fb9387555c41856f2f03
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-05-16 19:07:55 +0530

    feat: enhance CreateContentModal to add content with type selection and API integration

--- frontend/src/components/CreateContentModal.tsx
+++ frontend/src/components/CreateContentModal.tsx
@@ -1,14 +1,45 @@
+import { useRef, useState } from "react";
 import CrossIcon from "../icons/CrossIcon";
 import { Button } from "./Button";
 import InputComponent from "./InputComponent";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+type ContentType = "tweet" | "youtube" | "link" | "document";
+
 // controlled component
 export default function CreateContentModal({ isOpen, onClose }: ModalProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const linkRef = useRef<HTMLInputElement>(null);
+  const [type, setType] = useState<ContentType>("tweet");
+  async function addContent() {
+    const title = titleRef.current?.value;
+    const link = linkRef.current?.value;
+
+    console.log(link, type, title);
+
+    await axios.post(
+      `${BACKEND_URL}/api/v1/content`,
+      {
+        link,
+        type,
+        title,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+    console.log("content added.");
+    onClose();
+  }
   return (
     <div>
       {isOpen && (
@@ -16,34 +47,58 @@ export default function CreateContentModal({ isOpen, onClose }: ModalProps) {
           <div className=" bg-white rounded-lg p-4 flex flex-col items-center shadow-sm">
             <div className="flex justify-end w-full mb-4">
               <div className="cursor-pointer" onClick={onClose}>
-                <CrossIcon size="2xl"/>
+                <CrossIcon size="2xl" />
               </div>
             </div>
-            <InputComponent placeholder="Title" />
-            <InputComponent placeholder="Link" />
+            <InputComponent placeholder="Title" ref={titleRef} />
+            <InputComponent placeholder="Link" ref={linkRef} />
 
             <div className="flex justify-start w-full px-2 mt-2">
               <p>Type </p>
             </div>
-            <form className="flex p-2 mb-4 [&_input]:mr-1 [&_input]:w-5 [&_input]:cursor-pointer [&_label]:mr-4">
-              <input type="radio" name="content_type" value="tweet" />
+            <div className="flex p-2 mb-4 [&_input]:mr-1 [&_input]:w-5 [&_input]:cursor-pointer [&_label]:mr-4">
+              <input
+                type="radio"
+                name="content_type"
+                value="tweet"
+                checked={type === "tweet"}
+                onChange={() => setType("tweet")}
+              />
               <label>Tweet</label>
 
-              <input type="radio" name="content_type" value="youtube" />
+              <input
+                type="radio"
+                name="content_type"
+                value="youtube"
+                checked={type === "youtube"}
+                onChange={() => setType("youtube")}
+              />
               <label>Youtube</label>
 
-              <input type="radio" name="content_type" value="link" />
+              <input
+                type="radio"
+                name="content_type"
+                value="link"
+                checked={type === "link"}
+                onChange={() => setType("link")}
+              />
               <label>Link</label>
 
-              <input type="radio" name="content_type" value="document" />
+              <input
+                type="radio"
+                name="content_type"
+                value="document"
+                checked={type === "document"}
+                onChange={() => setType("document")}
+              />
               <label>Document</label>
-            </form>
+            </div>
 
             <Button
               variant="primary"
               size="md"
               text="Submit"
-              onClick={() => {}}
+              onClick={addContent}
             />
           </div>
         </div>

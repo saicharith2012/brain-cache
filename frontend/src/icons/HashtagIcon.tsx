0a1a6d9967daa37d8b0a2581abf97dceb1ed5756
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-05-17 07:53:47 +0530

    fix: correct stroke-width attribute to strokeWidth in HashtagIcon component

--- frontend/src/icons/HashtagIcon.tsx
+++ frontend/src/icons/HashtagIcon.tsx
@@ -6,7 +6,7 @@ export default function HashtagIcon(props: IconProps) {
       xmlns="http://www.w3.org/2000/svg"
       fill="current"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className={IconSizeVariants[props.size]}
     >

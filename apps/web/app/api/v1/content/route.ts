97f5933cba75cf0d43cf176dedb1a84bff2cc3ed
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-06-25 10:06:26 +0530

    boilerplate

---
+++ apps/web/app/api/v1/content/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function POST(req: NextRequest) {
+  return NextResponse.json({});
+}
+
+export function GET() {
+  return NextResponse.json({});
+}
+
+export function DELETE() {
+  return NextResponse.json({});
+}

d7877d51d81690c9c334bdac4a060e7e1a5ce1a2
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-09-17 16:55:43 +0530

    setup pdf ingestion pipeline to create vector embeddings

---
+++ apps/ingestion-worker/src/config.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const awsS3BucketName = process.env.AWS_S3_BUCKET_NAME
+export const awsRegion = process.env.AWS_REGION
+export const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID
+export const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+export const googleGenaiApiKey = process.env.GOOGLE_API_KEY
+export const qdrantUrl = process.env.QDRANT_URL
--- apps/ingestion-worker/src/index.ts
+++ apps/ingestion-worker/src/index.ts
@@ -1,10 +1,94 @@
 import { redisClient } from "@repo/redis/client";
+import { Readable } from "stream";
 import { Worker } from "bullmq";
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  awsAccessKeyId,
+  awsRegion,
+  awsS3BucketName,
+  awsSecretAccessKey,
+  googleGenaiApiKey,
+  qdrantUrl,
+} from "./config.js";
+import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { TaskType } from "@google/generative-ai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { url } from "inspector";
+
+const s3Client = new S3Client({
+  region: awsRegion,
+  credentials: {
+    accessKeyId: awsAccessKeyId!,
+    secretAccessKey: awsSecretAccessKey!,
+  },
+});
 
 const ingestionWorker = new Worker(
   "ingestion-queue",
   async (job) => {
-    console.log(`Processing job ${job.id} for memory ${job.data.contentId}`);
+    console.log(
+      `Processing job ${job.id} for memory ${JSON.stringify(job.data.contentId)}`
+    );
+
+    const response = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: awsS3BucketName,
+        Key: job.data.filePath,
+      })
+    );
+
+    if (!response.Body) {
+      throw new Error("no body in s3 response");
+    }
+
+    // fetching the pdf as a stream
+    const stream = response.Body as Readable;
+
+    const chunks = [];
+
+    for await (const chunk of stream) {
+      chunks.push(Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk));
+    }
+
+    // parsing the pdf
+    const fileBlob = new Blob(chunks, { type: "application/pdf" });
+
+    const loader = new WebPDFLoader(fileBlob);
+    const docs = await loader.load();
+
+    // chunking
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const texts = await textSplitter.createDocuments(
+      docs.map((doc) => doc.pageContent)
+    );
+
+    texts.forEach((text, index) => {
+      ((text.metadata.chunkIndex = index),
+        (text.metadata.sourceType = "document"),
+        (text.metadata.contentId = job.data.contentId));
+    });
+
+    // console.log(texts[0]);
+    const textsForEmbeddings = texts.map((text) => text.pageContent);
+
+    // creating vector embeddings
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+      model: "text-embedding-004",
+      taskType: TaskType.RETRIEVAL_DOCUMENT,
+      apiKey: googleGenaiApiKey,
+    });
+
+    const vectors = await embeddings.embedDocuments(textsForEmbeddings);
+    // console.log(vectors[0])
+
+    // storing in qdrant vector store
+    
   },
   { connection: redisClient }
 );
